76c940be8e917500e0d12872345908b3b2eed281
João Vitor <jvtreloso@gmail.com>
2026-01-14 19:09:20 -0300

    Criação da Sidebar responsiva com as suas categorias

--- Monetria/ViewModels/MainWindowViewModel.cs
+++ Monetria/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,16 @@
-﻿namespace Monetria.ViewModels;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace Monetria.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+    [ObservableProperty]
+    private bool _isPaneOpen = true;
+
+    [RelayCommand]
+    private void OpenPane()
+    {
+        IsPaneOpen = !IsPaneOpen;
+    }
 }
